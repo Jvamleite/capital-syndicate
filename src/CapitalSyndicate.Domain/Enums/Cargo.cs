ad4ea8415c7e5410c9b491cff0d19321e3829611
João Victor Amaral <joao.amaral@squadra.com.br>
2026-04-20 08:58:45 -0300

    Fix Alteração dos nomes nos enums para adequadar ao novo mapeamento de cargos e setores de mercado

--- src/CapitalSyndicate.Domain/Enums/Cargo.cs
+++ src/CapitalSyndicate.Domain/Enums/Cargo.cs
@@ -2,17 +2,17 @@
 {
     internal enum Cargo
     {
-        FACILITADOR,
-        RELACOES_PUBLICAS,
-        INVESTIDOR,
-        GESTOR_RH,
-        DIR_EXPANSAO,
-        ESTRATEGISTA,
-        ESP_ESG,
-        GEST_PORTFOLIO,
-        AUDITOR,
-        NEGOCIADOR_INT,
-        TRAINEE,
-        CONSULTOR_EXT
+        Facilitador,
+        DiretorDeComunicacao,
+        Investidor,
+        GestorDeRh,
+        DiretorDeExpansao,
+        DiretorDeOperacoes,
+        EspecialistaEsg,
+        GestorDePortifolio,
+        Auditor,
+        NegociadorInternacional,
+        Trainee,
+        ConsultorExterno
     }
 }
